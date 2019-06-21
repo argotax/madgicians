0265314dacbb329be45bd0a0984c9cbcf37f9d4b
Maxime Oble <moble@sigb.net>
2019-06-21 17:39:06 +0200

    scroll slider datesnew

--- index.php
+++ index.php
@@ -7,26 +7,24 @@
   <link rel="icon" href="images/madgicians-mini.ico">
   <link rel="stylesheet" href="vendors/fontawesome-free-5.9.0-web/css/all.css">
   <link rel="stylesheet" href="vendors/bootstrap-4.3.1-dist/css/bootstrap.min.css">
-  <script src="vendors/jquery/min.js"></script>
-  <script src="vendors/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="style/index.css">
+
   <title>Madgicians</title>
 </head>
 
-<body>
+<body >
 
-  <header>
+  <header id="header">
     <div class="header-block">
       <div class="header-wrapper">
 
         <div class="navbar">
           <nav>
-            <ul>
-              <li><a href="index.html" class="link home">Home</a></li>
-              <li><a href="bio.html" class="link bio">Bio</a></li>
-              <li><a href="dates.html" class="link dates">Dates</a></li>
-              <li><a href="media.html" class="link media-us">Media</a></li>
-              <li><a href="contact.html" class="link contact-us">Contact</a></li>
+            <ul class="op-sectionlist">
+              <li class="op-v-item"><a href="#home" class="op-v-link link home">Home</a></li>
+              <li class="op-v-item"><a href="#bio" class="op-v-link link bio">Bio</a></li>
+              <li class="op-v-item"><a href="#dates" class="op-v-link link dates">Dates</a></li>
+              <li class="op-v-item"><a href="#contact" class="op-v-link link contact-us">Contact</a></li>
             </ul>
           </nav>
         </div>
@@ -40,87 +38,128 @@
       </div>
   </header>
 
-  <main>
-    <div class="block body-block">
-      <div class="block home">
-        <div class="title">
-          <img src="images/madgicians-title.jpeg" alt="Madgicians-title">
-        </div>
+  <main id="main">
 
+    <div class="block body-block">
+      <div class="block home current" id="home">
+        <div class="title">
+          <div class="center-abso title-wrapper">
+            <img src="images/madgicians-title.jpeg" alt="Madgicians-title">
+          </div>
+        </div>
         <div class="image-wrapper" id="mini-logo">
-          <img src="images/madgicians-mini.png" alt="Madgicians-mini" class="madgicians-mini">
+          <div class="center-abso mini">
+            <img src="images/madgicians-mini.png" alt="Madgicians-mini" class="madgicians-mini">
+          </div>
         </div>
       </div>
 
-      <div class="block block-présentation">
-        <div class="videos-block" onclick="">
-          <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/508232325&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-        </div>
+      <div class="block block-presentation" id="bio">
 
-        <div class="photos-block" onclick="">
-
-          <div id="myCarousel" class="carousel slide" data-interval="3000" data-ride="carousel">
-            <!-- Carousel indicators -->
-            <ol class="carousel-indicators">
-              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-              <li data-target="#myCarousel" data-slide-to="1"></li>
-              <li data-target="#myCarousel" data-slide-to="2"></li>
-              <li data-target="#myCarousel" data-slide-to="3"></li>
-
-            </ol>
-            <!-- Wrapper for carousel items -->
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="images/1.jpg" alt="First Slide">
-
-              </div>
-              <div class="carousel-item">
-                <img src="images/2.jpg" alt="Second Slide">
-              </div>
-              <div class="carousel-item">
-                <img src="images/3.jpg" alt="Third Slide">
-              </div>
-              <div class="carousel-item">
-                <img src="images/4.jpg" alt="Third Slide">
-              </div>
+        <div class="text-block">
+            <div class="text-wrapper">
+              <p>
+                Quorum nescius ne firmissimum. Aut cillum veniam eram vidisse, quibusdam multos
+                iis aliquip coniunctione. Fugiat cernantur doctrina.E magna voluptate,
+                incididunt id malis, se elit admodum quibusdam, hic eu concursionibus. Offendit
+                ita nostrud. Aute ab constias sed quis. Ita iudicem id vidisse ad iudicem sed
+                export senserit.Aute de incididunt, et cupidatat exquisitaque, consequat irure cernantur, est
+                nisi illum e aliquip. Nulla laboris exquisitaque. Esse cupidatat deserunt, ex
+                quo duis esse dolor.Eram o quamquam hic illum. Nulla eu consequat est quis.
+                Summis consequat firmissimum, ita iudicem sed possumus se si quis incididunt et
+                eram a aliquip te se irure ubi irure, voluptate ex vidisse, e eram fabulas, se
+                ubi illum magna export. O export offendit probant ita eu sunt ingeniis laborum,
+                o anim philosophari ne aliquip irure anim ullamco minim sed ita esse enim id
+                incurreret, amet arbitror graviterque ut aute iudicem ad malis nisi aut ad
+                offendit firmissimum.
+              </p>
             </div>
-            <!-- Carousel controls -->
-            <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
-              <span class="carousel-control-prev-icon"></span>
-            </a>
-            <a class="carousel-control-next" href="#myCarousel" data-slide="next">
-              <span class="carousel-control-next-icon"></span>
-            </a>
           </div>
 
+        <div class="media-block">
+          <div class="music-block" onclick="">
+            <div class="music-wrapper">
+              <iframe width="100%" height="300" scrolling="yes" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/508232325&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+            </div>
+          </div>
+
+          <div class="photos-block" onclick="">
+          <div class="photos-wrapper">
+
+             <div id="myCarousel" class="carousel slide" data-interval="50000" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+                <li data-target="#myCarousel" data-slide-to="3"></li>
+                <li data-target="#myCarousel" data-slide-to="4"></li>
+                <li data-target="#myCarousel" data-slide-to="5"></li>
+                <li data-target="#myCarousel" data-slide-to="6"></li>
+                <li data-target="#myCarousel" data-slide-to="7"></li>
+                <li data-target="#myCarousel" data-slide-to="8"></li>
+                <li data-target="#myCarousel" data-slide-to="9"></li>
+
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img class="d-block w-100" src="images/1.jpg" alt="First Slide">
+                </div>
+                <div class="carousel-item">
+                  <img  class="d-block w-100" src="images/2.jpg" alt="Second Slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="images/3.jpg" alt="Third Slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="images/4.jpg" alt="Fourth Slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="images/5.jpg" alt="Fifth Slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="images/6.jpg" alt="Sixth Slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="images/7.jpg" alt="seventh Slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="images/8.jpg" alt="Heighth Slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="images/9.jpg" alt="Nineth Slide">
+                </div>
+              </div>
+              <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+              </a>
+              <a class="carousel-control-next" href="#myCarousel" data-slide="next">
+                <span class="carousel-control-next-icon"></span>
+              </a>
+            </div>
 
 
         </div>
-
-        <div class="block-text">
-          Quorum nescius ne firmissimum. Aut cillum veniam eram vidisse, quibusdam multos
-          iis aliquip coniunctione. Fugiat cernantur doctrina.E magna voluptate,
-          incididunt id malis, se elit admodum quibusdam, hic eu concursionibus. Offendit
-          ita nostrud. Aute ab constias sed quis. Ita iudicem id vidisse ad iudicem sed
-          export senserit.Aute de incididunt, et cupidatat exquisitaque, consequat irure cernantur, est
-          nisi illum e aliquip. Nulla laboris exquisitaque. Esse cupidatat deserunt, ex
-          quo duis esse dolor.Eram o quamquam hic illum. Nulla eu consequat est quis.
-          Summis consequat firmissimum, ita iudicem sed possumus se si quis incididunt et
-          eram a aliquip te se irure ubi irure, voluptate ex vidisse, e eram fabulas, se
-          ubi illum magna export. O export offendit probant ita eu sunt ingeniis laborum,
-          o anim philosophari ne aliquip irure anim ullamco minim sed ita esse enim id
-          incurreret, amet arbitror graviterque ut aute iudicem ad malis nisi aut ad
-          offendit firmissimum.
         </div>
+
+      </div></div>
+
+      <div class="block block-dates" id="dates">
+        <h2>Prochaines dates</h2>
+        <table>
+          <tr>
+            <th>Date</th>
+            <th>heure</th>
+            <th>lieu</th>
+          </tr>
+        </table>
       </div>
-
-    </div>
-
-    <div class="">
-
     </div>
   </main>
-  <footer>
+
+  <div id="loading"></div>
+
+
+  <footer id="footer">
 
     <div class="footer-wrapper">
       <div class="">
@@ -135,20 +174,50 @@
     </div>
 
   </footer>
+
+
+
+  <script src="vendors/jquery/min.js"></script>
+  <script src="vendors/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
   <script>
-  $(document).ready(function(){
-          $("#myCarousel").carousel();
+
+  $( document ).ready(function() {
+
+    setTimeout(function(){
+      $('body').css('overflow', 'scroll')
+      $('#loading').css('opacity', '0');
+      setTimeout(function(){
+        $('#loading').css('display', 'none');
+      }, 400);
+    }, 1400);
   });
 
   var elementPosition = $('#mini-logo').offset();
   $(window).scroll(function () {
-      if ($(window).scrollTop() > elementPosition.top) {
-          $('#mini-logo').css('position', 'fixed').css('top', '0');
-      } else {
-          $('#mini-logo').css('position', 'absolute');
-      }
-   });
+    if ($(window).scrollTop() > elementPosition.top-25) {
+      $('#mini-logo').css('position', 'fixed').css('top', '25px');
+    } else {
+      $('#mini-logo').css('position', 'absolute').css('top', '80%');
+    }
 
+  });
+
+  $(function() {
+    var scrollToAnchor = function( id ) {
+      var elem = $("div[id='"+ id +"']"); // on crée une balise d'ancrage
+      if ( typeof elem.offset()  === "undefined" ) { // on verifie si l'élément existe
+  		elem = $("#"+id); }
+      if ( typeof elem.offset()  !== "undefined") { // si l'élément existe, on continue
+        $('html, body').animate({
+          scrollTop: elem.offset().top }, 600 );} // on défini un temps de défilement de page
+    };
+    $("a").click(function( event ) { // on attache la fonction au click
+      if ( ($(this).attr("href").match("#")) && ($(this).attr("href")!="#myCarousel") ) { // on vérifie qu'il s'agit d'une ancre
+        event.preventDefault();
+        var href = $(this).attr('href').replace('#', '') // on scroll vers la cible
+        scrollToAnchor( href ); }
+    });
+  });
   </script>
 </body>
 
